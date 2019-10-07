b72bc2da3206016ba67d3f050aa5caa2e97603f8
Victor Dominguez <vd071090@gmail.com>
2019-10-07 03:09:54 -0400

    added objects to js

---
+++ assets/javascript/game.js
@@ -0,0 +1,35 @@
+/// Warriors///
+
+let kratos = {
+    name: "kratos",
+    health: 120,
+    attack: 8,
+    image: '<img src="assets/images/kratos-1.jpg" class = "image">'
+
+};
+
+let stranger = {
+    name: "stranger",
+    health: 100,
+    attack: 6,
+    image: '<img src="assets/images/stranger.jpg" class ="image">'
+};
+
+let thor = {
+    name: "thor",
+    health: 150,
+    attack: 10,
+    image: '<img src="assets/images/thor.jpg" class ="image">'
+};
+
+let zeus = {
+    name: "zeus",
+    health: 180,
+    attack: 12,
+    image: '<img src="assets/images/zeus.jpg" class ="image">'
+};
+
+
+
+
+
